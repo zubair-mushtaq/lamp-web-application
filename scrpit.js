a68abedc6111586e3ab54f8d3be719a5d571cec7
zubair-mushtaq <125160342+zubair-mushtaq@users.noreply.github.com>
2023-05-16 18:42:26 +0500

    Add files via upload
    lamp application

---
+++ scrpit.js
@@ -0,0 +1,26 @@
+let btn = document.querySelector("#btn");
+let light = document.querySelector("#body");
+let ab= document.querySelector("#hock");
+let abc = document.querySelector("#hhh");
+let ccv = document.querySelector("p");
+let bnn = document.querySelector("#blove")
+
+
+
+
+btn.onclick =function good(){
+    btn.classList.toggle("btnone");
+    light.classList.toggle("on");
+    ab.classList.toggle("goodone");
+    abc.classList.toggle("beauty");
+    ccv.classList.toggle("bnn");
+    bnn.classList.toggle("ghoo")
+    const userdata= hhtps//server/user.com
+if(userdata=="Zubair"){
+   // disply data
+}
+else {
+    console.error("no user found")
+}
+  
+}
